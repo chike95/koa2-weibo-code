f6f292ca3619d18cb7935461474998daf188b7cd
chike95 <148038535+chike95@users.noreply.github.com>
2024-01-30 16:58:09 +0800

    feat：session演示

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+const { REDIS_CONF } = require('./conf/db')
+
 const index = require('./routes/index')
 const users = require('./routes/users')
 
@@ -23,6 +27,23 @@ app.use(require('koa-static')(__dirname + '/public'))
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
+
+// 3-5 session 配置
+app.keys = ['UIsdf-7878￥@%']; // 设置session的密钥，用于加密session数据。
+app.use(session({
+  key: 'weibo.sid', // cookie name (default is koa.sid)
+  prefix: 'weibo:sess:', // 前缀：cookie prefix (default is empty string)
+  cookie: {
+    path: '/', // cookie的path (default is '/')
+    httpOnly: true, // 是否只允许http访问cookie (default is true)
+    maxAge: 24 * 60 * 60 * 1000, // cookie的过期时间 maxAge in ms 
+  },
+  // ttl: 24 * 60 * 60 * 1000,
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
+}))
+
 
 // logger
 app.use(async (ctx, next) => {
