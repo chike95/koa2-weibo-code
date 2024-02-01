390dd51c3ff839a258ab650a46e45e649ec7b612
chike95 <148038535+chike95@users.noreply.github.com>
2024-02-01 17:57:37 +0800

    test

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -18,6 +18,7 @@ router.post('/register', async (ctx, next) => {
 // 用户名是否存在
 router.post('/isExist', async (ctx, next) => {
     const { userName } = ctx.request.body
+    // 返回数据
     ctx.body = await isExist(userName)
 })
 
